0644ec37a28334f16aa47d0291eeda2985551ca2
LucasDev96 <lucaslong96@gmail.com>
2023-01-21 12:25:09 -0500

    Implement SetSpawnLocation For Enemies
    SetSpawnLocation should now be functional for EnemySpawnPooling. The PickSideToSpawn method picks 1 of 4 ints and then uses a switch case to choose a method to set the gameobjects position on one side of the screen.

--- Assets/Scripts/Enemies/EnemySpawnPooling.cs
+++ Assets/Scripts/Enemies/EnemySpawnPooling.cs
@@ -6,8 +6,17 @@ public class EnemySpawnPooling : SpawnPooling
 {
     public EnemySpawnPooling Instance { get; private set; }
 
+    [Header("Spawn List")]
     [SerializeField] private GameObject[] _enemies;
     private List<GameObject>[] _enemyLists;
+
+    [Header("Spawn Position Range")]
+    [SerializeField] private float topBottomXRange;
+    [SerializeField] private float topYPosition;
+    [SerializeField] private float bottomYPosition;
+    [SerializeField] private float leftRightYRange;
+    [SerializeField] private float leftXPosition;
+    [SerializeField] private float rightXPosition;
 
     private void Awake()
     {
@@ -25,6 +34,62 @@ public class EnemySpawnPooling : SpawnPooling
 
     public override void SetSpawnLocation(GameObject obj)
     {
-        throw new System.NotImplementedException();
+        PickSideToSpawn(obj);
+    }
+
+    void PickSideToSpawn(GameObject obj)
+    {
+        int num = Random.Range(0, 4);
+
+        switch (num)
+        {
+            case 0:
+                SpawnLeftSide(obj);
+                break;
+            case 1:
+                SpawnRightSide(obj);
+                break;
+            case 2:
+                SpawnTopSide(obj);
+                break;
+            case 3:
+                SpawnBottomSide(obj);
+                break;
+            default:
+                SpawnBottomSide(obj);
+                break;
+        }
+    }
+
+    void SpawnLeftSide(GameObject obj)
+    {
+        float xPos = leftXPosition;
+        float yPos = Random.Range(-leftRightYRange, leftRightYRange);
+
+        obj.transform.position = new Vector3(xPos, yPos, 0);
+    }
+
+    void SpawnRightSide(GameObject obj)
+    {
+        float xPos = rightXPosition;
+        float yPos = Random.Range(-leftRightYRange, leftRightYRange);
+
+        obj.transform.position = new Vector3(xPos, yPos, 0);
+    }
+
+    void SpawnTopSide(GameObject obj)
+    {
+        float xPos = Random.Range(-topBottomXRange, topBottomXRange);
+        float yPos = topYPosition;
+
+        obj.transform.position = new Vector3(xPos, yPos, 0);
+    }
+
+    void SpawnBottomSide(GameObject obj)
+    {
+        float xPos = Random.Range(-topBottomXRange, topBottomXRange);
+        float yPos = bottomYPosition;
+
+        obj.transform.position = new Vector3(xPos, yPos, 0);
     }
 }
