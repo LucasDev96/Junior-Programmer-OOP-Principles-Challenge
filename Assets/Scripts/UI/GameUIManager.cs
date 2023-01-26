1be5a60b0cf8fdb2b7cfaa64d5fe56bc9511b78b
LucasDev96 <lucaslong96@gmail.com>
2023-01-26 17:13:26 -0500

    Pause Functionality Added
    The game now pauses and unpauses when pressing escape. This brings up the pause menu when paused, and hides it again on unpause. Need to fix bug where player can still shoot during pause.

--- Assets/Scripts/UI/GameUIManager.cs
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -8,19 +8,29 @@ public class GameUIManager : MonoBehaviour
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI _scoreText;
     [SerializeField] private TextMeshProUGUI _livesText;
+    [SerializeField] private GameObject _gameOverScreen;
+    [SerializeField] private GameObject _gamePauseScreen;
 
     [Header("Variables")]
     [SerializeField] private int _startingLives;
-    private int _currentLives;
-    private int _score = 0;
     [SerializeField] private string _scoreString;
     [SerializeField] public string _livesString;
+
+    private int _currentLives;
+    private int _score = 0;
+    private bool isGameActive;
+    private bool isGamePaused;
 
     // Start is called before the first frame update
     void Start()
     {
-        _currentLives = _startingLives;
+        SetStartingVariables();
         SetScoreText(_score);
+    }
+
+    private void Update()
+    {
+        TogglePause();
     }
 
     // Set the score text, adding how many points have been gained
@@ -35,5 +45,50 @@ public class GameUIManager : MonoBehaviour
     {
         _currentLives--;
         _livesText.text = _livesString + _currentLives;
+    }
+
+    // Set a few variables on game start to ensure proper values
+    private void SetStartingVariables()
+    {
+        _currentLives = _startingLives;
+        isGameActive = true;
+        isGamePaused = false;
+        _gameOverScreen.SetActive(false);
+        _gamePauseScreen.SetActive(false);
+    }
+
+    // Pause the game if it's not already paused
+    private void PauseGame()
+    {
+
+        Debug.Log("game should be paused");
+        isGamePaused = true;
+        _gamePauseScreen.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+
+    // Unpause the game if it's paused
+    public void UnpauseGame()
+    {
+
+        Debug.Log("game should be unpaused");
+        isGamePaused = false;
+        _gamePauseScreen.SetActive(false);
+        Time.timeScale = 1;
+
+    }
+
+    // Toggle pause when the escape key is pressed
+    private void TogglePause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isGamePaused)
+        {
+            PauseGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isGamePaused)
+        {
+            UnpauseGame();
+        }
     }
 }
